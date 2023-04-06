6f83945f4725541a7fc5bf3f4a98951953d47653
AbdulWahabRaza0 <wahab@authornate.com>
2023-04-06 14:00:37 +0500

    adding files

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Wrapper } from "../../Common/Styled";
 import { Container } from "react-bootstrap";
 import { Spacer } from "../../Common/Spacer";
-import { P } from "../../Common/Typography";
+import { H1 } from "../../Common/Typography";
 import { calculateTimeLeft } from "../../utils/timeUtils";
 import "./style.scss";
 const Timer = () => {
@@ -16,34 +16,62 @@ const Timer = () => {
       <Container>
         <Spacer height="70px" />
         <Wrapper className="d-flex flex-row align-items-center justify-content-center mt-5 mb-5">
+          <H1></H1>
           <Wrapper>
             <div className="counter">
               <Wrapper className="counter-item">
-                <span className="value">
+                <span
+                  className="value"
+                  style={{
+                    borderBottomLeftRadius: "15px",
+                    borderBottomRightRadius: "15px",
+                    borderBottom: "1px solid white",
+                  }}
+                >
                   {String(timeLeft.days).padStart(2, "0")}
                 </span>
                 <span
                   className="label"
-                  style={{ borderTop: "2px solid white" }}
+                  style={{
+                    borderTopLeftRadius: "15px",
+                    borderTopRightRadius: "15px",
+                  }}
                 >
                   Days
                 </span>
               </Wrapper>
 
               <div className="counter-item">
-                <span className="value">
+                <span
+                  className="value"
+                  style={{
+                    borderBottomLeftRadius: "15px",
+                    borderBottomRightRadius: "15px",
+                    borderBottom: "1px solid white",
+                  }}
+                >
                   {String(timeLeft.hours).padStart(2, "0")}
                 </span>
                 <span
                   className="label"
-                  style={{ borderTop: "2px solid white" }}
+                  style={{
+                    borderTopLeftRadius: "15px",
+                    borderTopRightRadius: "15px",
+                  }}
                 >
                   Hours
                 </span>
               </div>
 
               <div className="counter-item">
-                <span className="value">
+                <span
+                  className="value"
+                  style={{
+                    borderBottomLeftRadius: "15px",
+                    borderBottomRightRadius: "15px",
+                    borderBottom: "1px solid white",
+                  }}
+                >
                   {String(timeLeft.minutes).padStart(2, "0")}
                 </span>
                 <span
@@ -63,6 +91,7 @@ const Timer = () => {
                   style={{
                     borderBottomLeftRadius: "15px",
                     borderBottomRightRadius: "15px",
+                    borderBottom: "1px solid white",
                   }}
                 >
                   {String(timeLeft.seconds).padStart(2, "0")}
